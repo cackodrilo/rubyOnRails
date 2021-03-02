01ee5914d4d78ba0187fc66433e5c1ca13b92b5d
cackodrilo <cmaturana.quesille@gmail.com>
2021-03-02 18:02:59 -0300

    nuevas integraciones

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,23 @@
 class ArticlesController < ApplicationController
+    before_action :find_article, except: [:new,:create,:index]
+
+    def index
+        @articles = Article.all
+    end
+
     def update
-        @article = Article.find(params[:id])
         @article.update(title: params[:article][:title], content: params[:article][:content])
         redirect_to @article
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def show
-        @article = Article.find(params[:id])
     end
     
     def new
         @article = Article.new
-        @article.title = 'Demo'
     end
 
     def create
@@ -24,9 +26,11 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to root_path
     end
 
+    def find_article
+        @article = Article.find(params[:id])
+    end
 end
